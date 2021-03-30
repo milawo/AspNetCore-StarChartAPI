ad355a9cabf663282c865108f7845caa211c7e2a
milawo <mlw@miwosolutions.dk>
2021-03-30 21:46:28 +0200

    GetById implemented

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -12,10 +12,37 @@ namespace StarChart.Controllers
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
-
+         
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+
+        }
+
+        [HttpGet("{id:int}", Name="GetById")]         
+        public IActionResult GetById(int id)
+        {               
+            var celestialObject = _context.CelestialObjects.Find(id);            
+
+            if (celestialObject == null)
+            {
+                return NotFound();
+            }
+
+            celestialObject.Satellites = new List<Models.CelestialObject>(); 
+
+            foreach (var co in _context.CelestialObjects)
+            {
+                if (co.OrbitedObjectId == null)                
+                    continue;
+                
+                if (co.OrbitedObjectId == id)
+                {
+                    celestialObject.Satellites.Add(co);
+                }                
+            }
+            
+            return Ok(celestialObject);
         }
     }
 }
